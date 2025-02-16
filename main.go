b64ad11c97061c3c75b336e3ae4baf114e69c55b
Darshan Rander <hey@darshanrander.com>
2025-02-16 10:03:04 +0000

    feat(cli): implement proper CLI

--- main.go
+++ main.go
@@ -5,10 +5,39 @@ import (
 	"os"
 	"os/user"
 
+	"github.com/SirusCodes/anti-lang/src/evaluator"
+	"github.com/SirusCodes/anti-lang/src/lexer"
+	"github.com/SirusCodes/anti-lang/src/object"
+	"github.com/SirusCodes/anti-lang/src/parser"
 	"github.com/SirusCodes/anti-lang/src/repl"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		printHelp()
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "repl":
+		runREPL()
+	case "run":
+		if len(os.Args) < 3 {
+			printHelp()
+			return
+		}
+		path := os.Args[2]
+		runFile(path)
+	case "help":
+		printHelp()
+	default:
+		printHelp()
+	}
+}
+
+func runREPL() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
@@ -17,4 +46,26 @@ func main() {
 	fmt.Printf("Hello, %s! to AntiLang!\n", user.Username)
 
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func runFile(path string) int {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	l := lexer.New(string(file))
+	p := parser.New(l)
+	ast := p.ParseProgram()
+	env := object.NewEnvironment()
+	evaluator.Eval(ast, env)
+
+	return 0
+}
+
+func printHelp() {
+	fmt.Println("Usage: anti-lang [command] [args]")
+	fmt.Println("Commands:")
+	fmt.Println("  repl - Start the AntiLang REPL")
+	fmt.Println("  run [filename] - Run an AntiLang file")
 }
---
+++ sample/add.al
@@ -0,0 +1,5 @@
+{a; b} add func [
+    ,a + b return
+]
+
+{{2; 4}add}print
