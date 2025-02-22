ed4f43604b5e0c8e4ba083ddff1ce12904de132d
Darshan Rander <hey@darshanrander.com>
2025-02-22 14:53:26 +0000

    chores: remove code suggestion from monaco

--- web/assets/monaco_config.js
+++ web/assets/monaco_config.js
@@ -139,53 +139,6 @@ require(['vs/editor/editor.main'], function () {
         }
     });
 
-    // Register a completion item provider for the new language
-    monaco.languages.registerCompletionItemProvider("antilang", {
-        provideCompletionItems: (model, position) => {
-            var word = model.getWordUntilPosition(position);
-            var range = {
-                startLineNumber: position.lineNumber,
-                endLineNumber: position.lineNumber,
-                startColumn: word.startColumn,
-                endColumn: word.endColumn,
-            };
-            var suggestions = [
-                {
-                    label: "simpleText",
-                    kind: monaco.languages.CompletionItemKind.Text,
-                    insertText: "simpleText",
-                    range: range,
-                },
-                {
-                    label: "testing",
-                    kind: monaco.languages.CompletionItemKind.Keyword,
-                    insertText: "testing(${1:condition})",
-                    insertTextRules:
-                        monaco.languages.CompletionItemInsertTextRule
-                            .InsertAsSnippet,
-                    range: range,
-                },
-                {
-                    label: "ifelse",
-                    kind: monaco.languages.CompletionItemKind.Snippet,
-                    insertText: [
-                        "if (${1:condition}) {",
-                        "\t$0",
-                        "} else {",
-                        "\t",
-                        "}",
-                    ].join("\n"),
-                    insertTextRules:
-                        monaco.languages.CompletionItemInsertTextRule
-                            .InsertAsSnippet,
-                    documentation: "If-Else Statement",
-                    range: range,
-                },
-            ];
-            return { suggestions: suggestions };
-        },
-    });
-
     monaco.editor.defineTheme('antilangTheme', {
         base: 'vs-dark',
         inherit: true,
